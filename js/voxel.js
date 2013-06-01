c8946e2efd644a70391e14b70b16784a6688f5d4
Max Ogden <max@maxogden.com>
2013-05-31 17:20:14 -0700

    store player state in db

--- js/voxel.js
+++ js/voxel.js
@@ -9,6 +9,23 @@ module.exports = {
   saveRegion: saveRegion
 }
 
+function getState(game) {
+  var state = {}
+  var target = game.controls.target()
+  state.player = {
+    position: target.avatar.position,
+    rotation: target.avatar.rotation
+  }
+  return state
+}
+
+function storeState(db, game, cb) {
+  if (!cb) cb = function noop(){}
+  return setInterval(function() {
+    db.put('gameState', getState(game), cb)
+  }, 5000)
+}
+
 function initGame(options) {
   $('.content').hide()
 
@@ -26,45 +43,72 @@ function initGame(options) {
   var chunkDimensions = [16, 16, 16]
   var gameChunkSize = 16
   
-  var game = require('voxel-hello-world')({
-    generateChunks: false,
-    texturePath: textures,
-    playerSkin: textures + '../player.png',
-    chunkSize: gameChunkSize,
-    chunkDistance: 4,
-    arrayType: Uint8Array,
-    worldOrigin: pos,
-    materials: materials
-  })
+  initDB(initRendering)
   
-  function startGame() {
-    game.paused = false
+  function initDB(cb) {
+    var level = voxelLevel('blocks', function(err) {
+      if (err) return cb(err.message)
+      level.db.get('gameState', { asBuffer: false }, function(err, state) {
+        cb(err, level, state)
+      })
+    })
+  }
+
+  function initRendering(err, level, state) {
+    if (err) return alert(err)
+    
+    var game = require('voxel-hello-world')({
+      generateChunks: false,
+      texturePath: textures,
+      playerSkin: textures + '../player.png',
+      chunkSize: gameChunkSize,
+      chunkDistance: 4,
+      arrayType: Uint8Array,
+      worldOrigin: pos,
+      materials: materials
+    })
+
     window.game = game // for console debugging
     var makeFly = fly(game)
-    makeFly(game.controls.target()).startFlying()
-    game.controls.target().avatar.position.copy({x: pos[0], y: pos[1], z: pos[2]})
+    game.flyer = makeFly(game.controls.target())
+    
+    var worldWorker = workerstream('world-worker-bundle.js')
+    worldWorker.on('data', function(data) {
+      if (data.ready && game.paused) return startGame(game, level, state, worldWorker)
+      var chunk = {
+        position: data.position,
+        voxels: new Uint8Array(data.buffer),
+        dims: data.dimensions
+      }
+      game.showChunk(chunk)
+    })
+    worldWorker.on('error', function(e) { console.log('err', e)})
+    worldWorker.write({ loadDB: 'blocks' })
   }
   
-  var worldWorker = workerstream('world-worker-bundle.js')
-  worldWorker.on('data', function(data) {
-    if (data.ready && game.paused) return startGame()
-    var chunk = {
-      position: data.position,
-      voxels: new Uint8Array(data.buffer),
-      dims: data.dimensions
-    }
-    game.showChunk(chunk)
-  })
-  worldWorker.on('error', function(e) { console.log('err', e)})
-  worldWorker.write({ loadDB: 'blocks' })
-  
-  game.voxels.on('missingChunk', function(p) {
-    worldWorker.write({
-      worldName: options.worldName,
-      position: p,
-      gameChunkSize: gameChunkSize
+  function startGame(game, level, state, worldWorker) {
+    game.voxels.on('missingChunk', function(p) {
+      worldWorker.write({
+        worldName: options.worldName,
+        position: p,
+        gameChunkSize: gameChunkSize
+      })
     })
-  })
+    
+    if (!state) state = {
+      player: {
+        position: {x: 0, y: 0, z: 0},
+        rotation: {x: 0, y: 0, z: 0}
+      }
+    }
+    game.paused = false
+    game.flyer.startFlying()
+    var avatar = game.controls.target().avatar
+    avatar.position.copy(state.player.position)
+    avatar.rotation.copy(state.player.rotation)
+    storeState(level.db, game)
+  }
+  
 }
 
 function saveRegion(buffer, worldName, regionX, regionZ, cb) {
