b0e3c8596831fb972b95d7e5ff35fb7d6ba8ca56
Max Ogden <max@maxogden.com>
2013-06-07 12:40:14 -0700

    turn on textures

--- js/voxel.js
+++ js/voxel.js
@@ -72,9 +72,9 @@ function initGame(options) {
       chunkDistance: 4,
       arrayType: Uint8Array,
       worldOrigin: pos,
-      materials: materials
+      materials: materials,
       // materials: ['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d', '#fd8d3c', '#fdae6b', '#fdd0a2', '#31a354', '#74c476', '#a1d99b', '#c7e9c0', '#756bb1', '#9e9ac8', '#bcbddc', '#dadaeb', '#636363', '#969696', '#bdbdbd', '#d9d9d9'],
-      // materialFlatColor: true
+      materialFlatColor: false
     })
 
     window.game = game // for console debugging
